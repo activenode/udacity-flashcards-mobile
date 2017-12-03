3b2107a5a870399d77cf88bc5d9f0cac92979961
David Lorenz <info@activenode.de>
2017-12-03 11:52:36 +0100

    feat: Animations implemented

--- components/QuizSummary.js
+++ components/QuizSummary.js
@@ -1,52 +1,77 @@
 import React from 'react';
-import { Text, View, Switch, StyleSheet } from 'react-native';
+import { Text, View, Switch, StyleSheet, Animated } from 'react-native';
 import commonStyles from '../utils/common-styles';
-import { Ionicons } from '@expo/vector-icons';
-import { blackyGrey, semiTransparentWhite } from '../utils/colors';
+import { blackyGrey, semiTransparentWhite , highlightGreen, coolGreen} from '../utils/colors';
 import Button, { BTN_ONDARK, BTN_ONDARK_SIMPLE } from './Button';
 
 
+export default class QuizSummary extends React.Component {
+  state = {
+    fadeResultsValue: new Animated.Value(0)
+  }
 
-export default function QuizSummary({
-  deckTitle,
-  totalCardsCount,
-  correctCardsCount,
-  onRestartQuiz,
-  onBackToDeck
-}) {
-  return (
-    <View style={[commonStyles.verticalCenteredPaddContainer, styles.container]}>
-      <Text style={[commonStyles.headline, styles.whiteText]}>Your Quiz Results</Text>
+  componentWillMount() {
+    this.state.fadeResultsValue.setValue(0);
+  }
 
-      <Text style={[commonStyles.headline2, styles.successRate, styles.whiteText]}>
-        { ((correctCardsCount / totalCardsCount) * 100).toFixed(0) }% Success Rate
-      </Text>
+  componentDidMount() {
+    Animated.timing(this.state.fadeResultsValue, {
+      toValue: 1,
+      duration: 700
+    }).start();
+  }
 
-      <View style={styles.metric}>
-        <Text style={[styles.metricValue, styles.whiteText]}>{totalCardsCount}</Text>
-        <Text style={[styles.greyText]}>{totalCardsCount === 1 ? 'Card' : 'Cards'} total</Text>
-      </View>
-      <View style={styles.metric}>
-        <Text style={[styles.metricValue, styles.whiteText]}>{correctCardsCount} / {totalCardsCount}</Text>
-        <Text style={[styles.greyText]}>correctly answered</Text>
-      </View>
+  render() {
+    const {
+      deckTitle,
+      totalCardsCount,
+      correctCardsCount,
+      onRestartQuiz,
+      onBackToDeck
+    } = this.props;
 
-      <View style={[commonStyles.btnContainer, { marginTop: 75 }]}>
-        <Button
-          onPress={onRestartQuiz}
-          text='Restart the Quiz'
-          type={BTN_ONDARK}
-          roundedBorders={{ topLeft: true, topRight: true, bottomRight: true, bottomLeft: true }} />
+    const roundedPercentsSuccess = ((correctCardsCount / totalCardsCount) * 100);
+
+    return (
+      <View style={[commonStyles.verticalCenteredPaddContainer, styles.container]}>
+        <Animated.View style={{ opacity: this.state.fadeResultsValue }}>
+          <Text style={[commonStyles.headline, styles.whiteText]}>Your Quiz Results</Text>
+
+          <Text style={[
+            commonStyles.headline2,
+            styles.successRate,
+            roundedPercentsSuccess < 100 ? styles.whiteText : styles.highlightText
+            ]}>
+            { roundedPercentsSuccess.toFixed(0) }% Success Rate
+          </Text>
+    
+          <View style={styles.metric}>
+            <Text style={[styles.metricValue, styles.whiteText]}>{totalCardsCount}</Text>
+            <Text style={[styles.greyText]}>{totalCardsCount === 1 ? 'Card' : 'Cards'} total</Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.metricValue, styles.whiteText]}>{correctCardsCount} / {totalCardsCount}</Text>
+            <Text style={[styles.greyText]}>correctly answered</Text>
+          </View>
+    
+          <View style={[commonStyles.btnContainer, { marginTop: 75 }]}>
+            <Button
+              onPress={onRestartQuiz}
+              text='Restart the Quiz'
+              type={BTN_ONDARK}
+              roundedBorders={{ topLeft: true, topRight: true, bottomRight: true, bottomLeft: true }} />
+          </View>
+          <View style={[commonStyles.btnContainer]}>
+            <Button
+              onPress={onBackToDeck}
+              text='Back to Deck'
+              type={BTN_ONDARK_SIMPLE}
+              roundedBorders={{ topLeft: true, topRight: true, bottomRight: true, bottomLeft: true }} />
+          </View>
+        </Animated.View>
       </View>
-      <View style={[commonStyles.btnContainer]}>
-        <Button
-          onPress={onBackToDeck}
-          text='Back to Deck'
-          type={BTN_ONDARK_SIMPLE}
-          roundedBorders={{ topLeft: true, topRight: true, bottomRight: true, bottomLeft: true }} />
-      </View>
-    </View>
-  )
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -61,6 +86,9 @@ const styles = StyleSheet.create({
   },
   whiteText: {
     color: 'white'
+  },
+  highlightText: {
+    color: coolGreen
   },
   greyText: {
     color: semiTransparentWhite
